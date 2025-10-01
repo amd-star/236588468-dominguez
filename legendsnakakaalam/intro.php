2f7f5c41e57287cb227d41181b41cdb8c42dd146
amd-star <amd8468@students.uc-bcf.edu.ph>
2025-10-01 11:26:40 +0800

    php exercises

---
+++ legendsnakakaalam/intro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Document</title>
+</head>
+<body>
+    <h3>Task 1. Introduce Yourself</h3>
+    <?php
+        $name="Axel"; 
+        $age="20";
+        echo "HI, I am " . $name;
+    ?>
+</body> 
+</html>
